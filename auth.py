e0de68c7e1772fc016d29efb0bc88c09b891f61c
= <s20c2007@bene.fit.ac.jp>
2022-10-31 22:57:11 +0900

    flash messages

--- auth.py
+++ auth.py
@@ -1,4 +1,5 @@
 from flask import *
+import flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager,login_user,logout_user
 from app import User, db
@@ -16,10 +17,11 @@ def login():
         password = request.form.get("password")
         remember = True if request.form.get("remember") else False
         user = User.query.filter_by(email=email).first()
-        print(user.id)
         if not user or not check_password_hash(user.password, password):
+            flash("ログインが失敗しました")
             return redirect(url_for("auth.login"))
         login_user(user, remember=remember)
+        flash("ログイン完了しました")
         return redirect(url_for("profile", user_id=user.id))
 
 @auth.route("/signup" ,methods=["GET","POST"])
@@ -32,7 +34,10 @@ def signup():
         password = request.form.get("password")
         user = User.query.filter_by(email=email).first() 
         if user:
+            flash("")
             return redirect(url_for("auth.signup"))
+            
+        
         new_user = User(email=email, name=name, password=generate_password_hash(password, method="sha256"))
         db.session.add(new_user)
         db.session.commit()
@@ -40,5 +45,6 @@ def signup():
         
 @auth.route("/logout")
 def logout():
+    flash("ログアウトしました")
     logout_user()
     return redirect(url_for("index"))
