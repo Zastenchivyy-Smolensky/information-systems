1406f03f91a916da869c0d03adbcf4e09669aa03
= <s20c2007@bene.fit.ac.jp>
2023-07-27 22:21:32 +0900

    ..

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from email.policy import default
 from flask import *
 app = Flask(__name__)
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin,LoginManager,login_required, current_user
-from werkzeug.security import generate_password_hash
+from flask_login import UserMixin,LoginManager,login_required, current_user,login_user,logout_user
+from werkzeug.security import generate_password_hash,check_password_hash
 import os
 import secrets
 from sqlalchemy import or_
@@ -162,8 +162,47 @@ def delete(id):
     return redirect(url_for("index"))
 
 
-# import auth
-# app.register_blueprint(auth)
+
+@app.route("/login", methods=["GET","POST"])
+def login():
+    if request.method == "GET":
+        return render_template("auth/login.html")
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+        remember = True if request.form.get("remember") else False
+        user = User.query.filter_by(email=email).first()
+        if not user or not check_password_hash(user.password, password):
+            flash("ログインが失敗しました")
+            return redirect(url_for("auth.login"))
+        login_user(user, remember=remember)
+        flash("ログイン完了しました")
+        return redirect(url_for("profile", user_id=user.id))
+
+@app.route("/signup" ,methods=["GET","POST"])
+def signup():
+    if request.method=="GET":
+        return render_template("auth/signup.html")
+    if request.method == "POST":
+        email = request.form.get("email")
+        name = request.form.get("name")
+        password = request.form.get("password")
+        user = User.query.filter_by(email=email).first() 
+        if user:
+            flash("")
+            return redirect(url_for("auth.signup"))
+            
+        
+        new_user = User(email=email, name=name, password=generate_password_hash(password, method="sha256"))
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for("auth.login"))
+        
+@app.route("/logout")
+def logout():
+    flash("ログアウトしました")
+    logout_user()
+    return redirect(url_for("index"))
 
 @app.route("/profile/<int:user_id>")
 @login_required
